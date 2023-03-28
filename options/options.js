8c3a1b608ac4f9c00a26c24efa09579f464609de
Crowdbotics <team@crowdbotics.com>
2023-03-28 09:08:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "anrariapp-39579",
+  url: "https://anrariapp-39579.botics.co",
+  api: "https://anrariapp-39579.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
